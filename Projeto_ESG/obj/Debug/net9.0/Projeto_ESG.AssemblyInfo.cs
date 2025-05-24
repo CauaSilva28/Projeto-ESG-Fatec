bc14a58efc72adef5bcd8ead173c6805eca774bc
MKevin2 <matheus.kevinsantos2018@gmail.com>
2025-05-24 17:51:37 -0300

    Adicionando o menu e as imagens abstratas

--- Projeto_ESG/obj/Debug/net9.0/Projeto_ESG.AssemblyInfo.cs
+++ Projeto_ESG/obj/Debug/net9.0/Projeto_ESG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f057fc929f851810d17ebeb9c872ad13004f3bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7b71d44bcaa4598d2d93da794f6e5fbd26973e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
