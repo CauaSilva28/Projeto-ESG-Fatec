13cd384bfb1d2ea86706119c07054be7f42353dd
CauaSilva28 <127631052+CauaSilva28@users.noreply.github.com>
2025-06-14 23:44:47 -0300

    finalizando front da tela perfil

--- Projeto_ESG/obj/Debug/net9.0/Projeto_ESG.AssemblyInfo.cs
+++ Projeto_ESG/obj/Debug/net9.0/Projeto_ESG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee800ee404757d744f193054172a789295b2620a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3afb4dfe125f99ac65dd263960d20b2a5a18a9f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
