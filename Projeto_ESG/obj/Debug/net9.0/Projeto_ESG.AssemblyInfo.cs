51b2aad8169f1dff3e3b4c3f776e117248edc0cd
MKevin2 <matheus.kevinsantos2018@gmail.com>
2025-06-17 20:38:24 -0300

    Comentando os códigos da index.css e iniciando da login.css

--- Projeto_ESG/obj/Debug/net9.0/Projeto_ESG.AssemblyInfo.cs
+++ Projeto_ESG/obj/Debug/net9.0/Projeto_ESG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f703d1294d2ef571beb9c856418b2823d7ece40f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed675ab29d6fdfea68a63ca21974a2e2c04aa11")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
