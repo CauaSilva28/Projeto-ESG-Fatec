fefe31953fb4dd8b35ee54e32f9b2d695a1500bd
MKevin2 <matheus.kevinsantos2018@gmail.com>
2025-06-23 10:52:43 -0300

    Finalizando tela de login e cadastro

--- Projeto_ESG/obj/Debug/net9.0/Projeto_ESG.AssemblyInfo.cs
+++ Projeto_ESG/obj/Debug/net9.0/Projeto_ESG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08bc85434223c93db604573a6558e7658d46e20f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431eb1eb039c88464d54388c5a5dd91319275c25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_ESG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
